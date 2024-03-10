18df66b542db492dc90e867e30022090881d4408
Szymon Bacanski <72625642+sbacanski0730@users.noreply.github.com>
2024-03-10 23:07:00 +0100

    pr/register-endpoint-validation
    Validation added to register endpoint.
    Zod was installed as validation library.

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,14 +1,14 @@
 import AuthController from '../controllers/AuthController';
 import { Application } from 'express';
 import authController from '../controllers/AuthController';
-
+import validateRegisterRequest from '../validators/validateRegisterRequest';
 class AuthRoutes {
 	private authController = new AuthController();
 
 	public constructor() {}
 
 	public createAuthRoutes = (app: Application) => {
-		app.route('/api/auth/register').post(this.authController.userRegisterHandler);
+		app.route('/api/auth/register').post(validateRegisterRequest, this.authController.userRegisterHandler);
 	};
 }
 
